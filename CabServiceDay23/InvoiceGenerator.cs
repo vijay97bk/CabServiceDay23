c60b69880641f0ae33a89f69891df6c3c0cab232
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-12 12:48:50 +0530

    [vijay]FareCalculation

---
+++ CabServiceDay23/InvoiceGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabServiceDay23
+{
+    class InvoiceGenerator
+    {
+        readonly int pricePerKilometer = 10;
+        readonly int pricePerMinute = 1;
+        readonly int minimumFare = 5;
+        public double returnTotalFare(double distance, double time)
+        {
+            return Math.Max(minimumFare, distance * pricePerKilometer + time * pricePerMinute);
+        }
+    }
+}
